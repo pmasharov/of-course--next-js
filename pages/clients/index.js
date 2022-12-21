83333828d30b773d32a8aa20ff89cdbdf9bdb9c3
pmasharov <2386444@gmail.com>
2022-12-21 23:38:15 +0300

    add Link; different way to specify Link's href prop value;

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,28 @@
+import Link from "next/link";
+
 const ClientPage = () => {
+	const clients = [
+		{ id: "first", name: "First" },
+		{ id: "second", name: "Second" },
+	];
 	return (
 		<div>
 			<h1>The clients page</h1>
+			<ul>
+				{clients.map((client) => (
+					<li key={client.id}>
+						{/* <Link href={`/clients/${client.id}`}>{client.name}</Link> */}
+						<Link
+							href={{
+								pathname: "/clients/[clientId]",
+								query: { clientId: "first" },
+							}}
+						>
+							{client.name}
+						</Link>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
