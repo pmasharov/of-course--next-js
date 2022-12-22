cff3c9f9e58fb13d2fcf48bf299b7aaff45619c3
pmasharov <2386444@gmail.com>
2022-12-22 14:15:06 +0300

    add common routes and pages

---
+++ pages/events/[eventId].js
@@ -0,0 +1,9 @@
+const EventDetailsPage = () => {
+	return (
+		<div>
+			<h1>Event Details Page</h1>
+		</div>
+	);
+};
+
+export default EventDetailsPage;
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+const AllEventsPage = () => {
+	return (
+		<div>
+			<h1>All events page</h1>
+		</div>
+	);
+};
+
+export default AllEventsPage;
