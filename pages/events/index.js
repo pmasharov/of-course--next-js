ffd2785563f24497e33e2f6243c9c155491a928e
pmasharov <2386444@gmail.com>
2022-12-23 17:17:22 +0300

    add events list to all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,10 @@
+import { getAllEvents } from "../../dummy-data";
+import { EventList } from "@components/events/EventList";
+
 const AllEventsPage = () => {
-	return (
-		<div>
-			<h1>All events page</h1>
-		</div>
-	);
+	const events = getAllEvents();
+
+	return <EventList events={events} />;
 };
 
 export default AllEventsPage;
